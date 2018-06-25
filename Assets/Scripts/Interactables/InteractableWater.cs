ab47c261975d7ec6688b67aad50a0082a551b309
Anjum Shakeel Janjua <Anjumshakeel1234@gmail.com>
2018-06-25 14:43:52 +0500

    New Scripts Added and instances created in Game Manager

---
+++ Assets/Scripts/Interactables/InteractableWater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableWater : Interactables {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    #region Private Variables
+    private EnemyManager m_enemyManager;
+    #endregion
+}
